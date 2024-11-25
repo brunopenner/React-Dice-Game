c948fbf37e1db33ac379f331bd0a4f890673bc14
Bruno Penner <brunopenner01@gmail.com>
2024-11-25 09:53:21 +0800

    #660. The LuckyN Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Dice from './Dice'
+import LuckyN from './LuckyN'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Dice dice={[3,6,1]} />
-      <Dice dice={[1,2,3]} />
+       <LuckyN />
     </>
   )
 }
