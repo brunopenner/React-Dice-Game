7cdc22c260d0a847ce053d88b0fb59fd2006a87d
Bruno Penner <brunopenner01@gmail.com>
2024-11-26 15:03:22 +0800

    #664. Practice Passing State Updating Functions

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
 import "./Box.css"
-export default function Box(isActive, toggle) {
+export default function Box({isActive, toggle}) {
     return <div onClick={toggle} className="Box" style={{backgroundColor: isActive ? "red" : "black"}}></div>
 }
